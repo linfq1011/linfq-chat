e0d232922009689bf67a165a8d782a44bf2da0eb
linfq <linfq1011@users.noreply.github.com>
2019-07-22 20:35:46 +0800

    前端项目

---
+++ linfq-chat-h5/js/app.js
@@ -0,0 +1,65 @@
+window.app = {
+	
+	/**
+	 * netty服务后端发布的url地址
+	 */
+	nettyServerUrl: 'ws://192.168.1.106:8088/ws',
+	
+	/**
+	 * 后端服务发布的url地址
+	 */
+	serverUrl: 'http://192.168.1.106:8080',
+	
+	/**
+	 * 图片服务器的url地址
+	 */
+	imgServerUrl: '',
+	
+	/**
+	 * 判断字符串是否为空
+	 * @param {Object} str
+	 * true：不为空
+	 * false：为空
+	 */
+	isNotNull: function(str) {
+		if (str != null && str != "" && str != undefined) {
+			return true;
+		}
+		return false;
+	},
+	
+	/**
+	 * 封装消息提示框，默认mui的不支持剧中和自定义icon，所以使用h5+
+	 * @param {Object} msg
+	 * @param {Object} type
+	 */
+	showToast: function(msg, type) {
+		plus.nativeUI.toast(msg, 
+			{icon: "image/" + type + ".png", verticalAlign: "center"})
+	},
+	
+	/**
+	 * 保存用户的全局对象
+	 * @param {Object} user
+	 */
+	setUserGlobalInfo: function(user) {
+		var userInfoStr = JSON.stringify(user);
+		plus.storage.setItem("userInfo", userInfoStr);
+	},
+	
+	/**
+	 * 获取用户的全局对象
+	 */
+	getUserGlobalInfo: function() {
+		var userInfoStr = plus.storage.getItem("userInfo");
+		return JSON.parse(userInfoStr);
+	},
+	
+	/**
+	 * 登出后，移除用户全局对象
+	 */
+	userLogout: function() {
+		plus.storage.removeItem("userInfo");
+	}
+	
+}
