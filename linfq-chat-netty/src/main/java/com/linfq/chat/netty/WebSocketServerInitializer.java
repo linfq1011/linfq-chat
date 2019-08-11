d17d4ff61886a2e16e27917c3109b90060c42b3a
linfq <linfq1011@users.noreply.github.com>
2019-08-11 17:46:45 +0800

    实现心跳监测。

--- linfq-chat-netty/src/main/java/com/linfq/chat/netty/WebSocketServerInitializer.java
+++ linfq-chat-netty/src/main/java/com/linfq/chat/netty/WebSocketServerInitializer.java
@@ -7,6 +7,7 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.handler.timeout.IdleStateHandler;
 
 /**
  * WebSocketServerInitializer.
@@ -28,6 +29,17 @@ public class WebSocketServerInitializer extends ChannelInitializer<SocketChannel
 		pipeline.addLast(new HttpObjectAggregator(1024 * 64));
 
 		// ====================== 以上是用于支持http协议 ======================
+
+
+		// ====================== 增加心跳支持 start ======================
+
+		// 针对客户端，如果在1分钟没有想服务端发送读写心跳（ALL），则主动断开
+		// 如果是读空闲或者写空闲，不处理
+		pipeline.addLast(new IdleStateHandler(10, 20, 60));
+		// 自定义空闲状态监测
+		pipeline.addLast(new HeartBeadHandler());
+
+		// ====================== 增加心跳支持 end ======================
 
 		// websocket 服务器处理的协议，用于指定给客户端连接访问的路由：/ws
 		// 本handler会帮你处理一些繁重的复杂的事
